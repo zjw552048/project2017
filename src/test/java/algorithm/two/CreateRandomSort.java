7276f499b467e86b4c7d4c9490c43c7a5689aced
zjw552048 <505113236@qq.com>
2017-07-06 19:15:36 +0800

    20170706 company

--- src/test/java/algorithm/two/CreateRandomSort.java
+++ src/test/java/algorithm/two/CreateRandomSort.java
@@ -12,17 +12,15 @@ public class CreateRandomSort {
 		//System.out.println(Math.pow(2, 32));
 		int m = 100;
 		int n = (int)Math.pow(2, 20);
-		int times = 20;
-		while(times>0){
-			System.out.println(Arrays.toString(sort(m, n)));
-			System.out.println(sort2(m, n));
-			times--;
-		}
+		
+//		System.out.println(Arrays.toString(sort(m, n)));
+//		System.out.println(sort2(m, n));
+		System.out.println(Arrays.toString(sort3(m, n)));
 	}
 	
 	/**
-	 * 从0~n-1随机输出m个有序整数
-	 * 且m>n
+	 * 数组形式,从0~n-1随机输出m个有序整数,且m>n
+	 * 利用random%(n) > m,每个值被选取的概率都为m/n
 	 * @param m
 	 * @param n
 	 * @return
@@ -32,7 +30,7 @@ public class CreateRandomSort {
 		
 		int[] result = new int[m];
 		int temp = n;
-		for(int i=0;i<temp;i++){
+		for(int i=0;i<temp&m>0;i++){
 			int random = (int)(Math.random()*(n-i));
 			int value = random%(n-i);
 			if(value<m){
@@ -46,6 +44,12 @@ public class CreateRandomSort {
 		return result; 
 	}
 	
+	/**
+	 * 集合形式,随机获取的值不存在于集合中就添加，选取完成后排序
+	 * @param m
+	 * @param n
+	 * @return
+	 */
 	public static List<Integer> sort2(int m, int n){
 		long start = System.currentTimeMillis();
 		
@@ -59,6 +63,27 @@ public class CreateRandomSort {
 			}
 		}
 		Collections.sort(result);
+		
+		long end = System.currentTimeMillis();
+		System.out.println(end-start);
+		return result;
+	}
+	
+	public static int[] sort3(int m, int n){
+		long start = System.currentTimeMillis();
+		
+		int[] result = new int[n];
+		for(int i=0;i<n;i++){
+			result[i] = i;
+		}
+		for(int i=0;i<m;i++){
+			int random = (int)(Math.random() * n);
+			int temp = result[i];
+			result[i] = result[random];
+			result[random] = temp;
+		}
+		result= Arrays.copyOf(result, m);
+		Arrays.sort(result);
 		
 		long end = System.currentTimeMillis();
 		System.out.println(end-start);
